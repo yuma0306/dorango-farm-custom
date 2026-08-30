a20c6ab8d3d509eb144f96216c119b1ec99425f6
yuma0306 <yuma0306895@gmail.com>
2026-08-30 17:21:34 +0700

    修正

--- wp-content/themes/dorango-farm-custom/include/gtm-body.php
+++ wp-content/themes/dorango-farm-custom/include/gtm-body.php
@@ -1,4 +1,6 @@
+<?php if (!is_user_logged_in()) : ?>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPWXLLW"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+<?php endif; ?>
